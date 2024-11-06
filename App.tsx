bc4dff738e19f41a455d6a0bc594f823cf930e58
DavitVosk <dvoskanyan2@gmail.com>
2024-11-07 00:16:19 +0400

    Android: fix status bar style

--- App.tsx
+++ App.tsx
@@ -2,10 +2,15 @@ import {ThemeProvider} from 'styled-components';
 
 import AppNavigation from '@src/navigation';
 import {theme} from '@src/constants/theme';
+import {StatusBar} from 'react-native';
 
 export default () => {
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={theme.colors.background}
+      />
       <AppNavigation />
     </ThemeProvider>
   );
--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import {StatusBar} from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
@@ -11,17 +10,13 @@ const Stack = createStackNavigator<StackParamList>();
 
 const AppNavigation = () => {
   return (
-    <>
-      <StatusBar barStyle="light-content" />
-
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName={Routes.Connect}
-          screenOptions={{headerShown: false}}>
-          <Stack.Screen name={Routes.Connect} component={Connect} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName={Routes.Connect}
+        screenOptions={{headerShown: false}}>
+        <Stack.Screen name={Routes.Connect} component={Connect} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
