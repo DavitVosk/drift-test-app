3eb22bead5625fc226c15d5b868732c64003f7bc
DavitVosk <dvoskanyan2@gmail.com>
2024-11-09 19:22:58 +0400

    Use default ABCMarfaVariableVFTrial font

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ import 'react-native-get-random-values';
 import {Buffer} from 'buffer';
 global.Buffer = Buffer;
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, Text} from 'react-native';
+import setDefaultProps from 'react-native-simple-default-props';
+
 import App from './App';
 import {name as appName} from './app.json';
+
+setDefaultProps(Text, {
+  style: [{fontFamily: 'ABCMarfaVariableVFTrial'}],
+});
 
 AppRegistry.registerComponent(appName, () => App);
